a65d1b9ed6b7ae8804f998e89afa795c554b2482
Levente Bagi <bagilevi@gmail.com>
2018-09-08 15:16:20 +0100

    chore: use CDN

--- public/init.js
+++ public/init.js
@@ -3,12 +3,12 @@ console.log('init loaded');
 require.config({
   baseUrl: MEMONITE_PLUGIN_PATH,
   paths: {
-    'jquery': '/jquery/jquery-3.3.1.min',
-    'lodash': '/lodash/lodash.min',
+    'jquery': 'https://code.jquery.com/jquery-3.3.1.min',
+    'lodash': 'https://cdn.jsdelivr.net/npm/lodash@4.17.10/lodash.min',
   }
 })
 console.log('MEMONITE_VERSION', MEMONITE_VERSION)
 
-define([`/memonite-core-v${MEMONITE_VERSION}.js`], () => {
+define([`${MEMONITE_PLUGIN_PATH}/memonite-core-v${MEMONITE_VERSION}.js`], () => {
 
 })
