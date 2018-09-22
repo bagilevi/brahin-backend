e1035f4191cf50062482f4b6e63b58ab87f544a6
Levente Bagi <bagilevi@gmail.com>
2018-09-22 08:31:44 +0100

    chore: add postgres

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,17 @@
 require_relative 'boot'
 
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+# require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
 require "action_controller/railtie"
+# require "action_mailer/railtie"
+# require "action_view/railtie"
+# require "action_cable/engine"
 require "sprockets/railtie"
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,14 +28,5 @@ module Brahin
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    config.assets.precompile += %w(
-      init.js
-      ui.js
-      spa.js
-      linking.js
-      jquery-3.3.1.min.js
-      first-v0.0.1.js
-    )
   end
 end
