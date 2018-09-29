12bd843cf6bc782538885345bc5e6e36c9a38289
Levente Bagi <bagilevi@gmail.com>
2018-09-29 12:51:29 +0100

    v0.3.1

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,6 @@ require "sprockets/railtie"
 Bundler.require(*Rails.groups)
 
 module Brahin
-  VERSION = '0.3.0'.freeze
-
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
